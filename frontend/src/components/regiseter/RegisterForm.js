22aba0c2c9e659b6ef2e9f70c2ad6e1ffc0d0f31
Mohamed Ramadan <mr.uiux.dev@gmail.com>
2023-12-29 23:19:45 +0200

    Register form validation

--- frontend/src/components/regiseter/RegisterForm.js
+++ frontend/src/components/regiseter/RegisterForm.js
@@ -1,6 +1,7 @@
 import { Form, Formik } from "formik";
 import "./style.scss";
 import Input from "../input";
+import * as Yup from "yup";
 import { useState } from "react";
 
 const RegisterForm = () => {
@@ -10,10 +11,24 @@ const RegisterForm = () => {
     email: "",
     password: "",
     gender: "",
-    bYear: "",
-    bMonth: "",
-    bDay: "",
+    bYear: new Date().getFullYear(),
+    bMonth: new Date().getMonth() + 1,
+    bDay: new Date().getDate(),
   });
+
+  const { firstName, lastName, email, password, gender, bYear, bMonth, bDay } =
+    register;
+
+  const years = Array.from(
+    new Array(108),
+    (val, i) => new Date().getFullYear() - i
+  );
+
+  const months = Array.from(new Array(12), (val, i) => i + 1);
+
+  const getDays = () => new Date(bYear, bMonth, 0).getDate();
+
+  const days = Array.from(new Array(getDays()), (val, i) => i + 1);
 
   const handleRegisterChange = (e) => {
     e.preventDefault();
@@ -21,9 +36,31 @@ const RegisterForm = () => {
     const { value, name } = e.target;
 
     setRegister({ ...register, [name]: value });
-
-    console.log(register);
   };
+
+  const registerValidationSchema = Yup.object({
+    firstName: Yup.string()
+      .required("First name is required")
+      .min(2, "Name must between 2 and 16 characters")
+      .max(16, "Name must between 2 and 16 characters")
+      .matches(/^[aA-zZ]+$/, "Number and special characters are not allowed"),
+    lastName: Yup.string()
+      .required("Last name is required")
+      .min(2, "Name must between 2 and 16 characters")
+      .max(16, "Name must between 2 and 16 characters")
+      .matches(/^[aA-zZ]+$/, "Number and special characters are not allowed"),
+    email: Yup.string()
+      .required(
+        "You'll need this when you log in and if ever need to reset your password"
+      )
+      .email("Enter a valid email"),
+    password: Yup.string()
+      .required(
+        "Enter a combination of at least six number, letters and punctuation marks(such as ! and &)"
+      )
+      .min(6, "Password must be at least 6 characters")
+      .max(36, "Password can't be more than 36 characters"),
+  });
 
   return (
     <div className="blur">
@@ -33,7 +70,20 @@ const RegisterForm = () => {
           <span>Sign Up</span>
           <span>It's quick and easy</span>
         </div>
-        <Formik enableReinitialize>
+        <Formik
+          enableReinitialize
+          initialValues={{
+            firstName,
+            lastName,
+            email,
+            password,
+            gender,
+            bYear,
+            bMonth,
+            bDay,
+          }}
+          validationSchema={registerValidationSchema}
+        >
           {(formik) => (
             <Form className="register_form">
               <Input
@@ -65,14 +115,38 @@ const RegisterForm = () => {
                   Date of birth <i className="info_icon"></i>
                 </div>
                 <div className="reg_grid">
-                  <select name="bDay" onChange={handleRegisterChange}>
-                    <option value="">15</option>
+                  <select
+                    name="bDay"
+                    value={bDay}
+                    onChange={handleRegisterChange}
+                  >
+                    {days.map((day) => (
+                      <option key={day} month={day}>
+                        {day}
+                      </option>
+                    ))}
                   </select>
-                  <select name="bMonth" onChange={handleRegisterChange}>
-                    <option value="">15</option>
+                  <select
+                    name="bMonth"
+                    value={bMonth}
+                    onChange={handleRegisterChange}
+                  >
+                    {months.map((month) => (
+                      <option value={month} key={month}>
+                        {month}
+                      </option>
+                    ))}
                   </select>
-                  <select name="bYear" onChange={handleRegisterChange}>
-                    <option value="">15</option>
+                  <select
+                    name="bYear"
+                    value={bYear}
+                    onChange={handleRegisterChange}
+                  >
+                    {years.map((year) => (
+                      <option key={year} value={year}>
+                        {year}
+                      </option>
+                    ))}
                   </select>
                 </div>
               </div>
@@ -82,17 +156,35 @@ const RegisterForm = () => {
                 </div>
                 <div className="reg_grid">
                   <label htmlFor="male">
-                    Male <input type="radio" name="gender" id="male" />
+                    <span> Male</span>
+                    <input
+                      type="radio"
+                      name="gender"
+                      id="male"
+                      value="male"
+                      onChange={handleRegisterChange}
+                    />
                   </label>
                   <label htmlFor="female">
-                    Female <input type="radio" name="gender" id="female" />
+                    <span>Female</span>
+                    <input
+                      type="radio"
+                      name="gender"
+                      id="female"
+                      value="female"
+                      onChange={handleRegisterChange}
+                    />
                   </label>
                 </div>
               </div>
               <div className="reg_info">
-                By clicking, sign up, you agree to our terms, data policy and
-                cookie policy. You may receive notification from us and can opt
-                out at any time
+                By clicking, sign up, you agree to our{" "}
+                <span>terms, data policy</span> and
+                <span>cookie policy</span>. You may receive notification from us
+                and can opt out at any time
+              </div>
+              <div className="reg_btn_wrapper">
+                <button className="blue_btn signup_btn">Sign up</button>
               </div>
             </Form>
           )}
